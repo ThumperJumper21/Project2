54edda91dd17f2b267669814c3e7682caf88b4b0
Elijah White <144889100+ThumperJumper21@users.noreply.github.com>
2024-12-11 13:47:44 -0500

    Delete Section1Part3 directory

--- Section1Part3/demo/src/main/java/project/RunLinear.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public class RunLinear {
-    public static void main(String[] args) {
-        // Create an instance of the Linear class and invoke the run method
-        Linear linearFunction = new Linear();
-        linearFunction.run();
-    }
-}
